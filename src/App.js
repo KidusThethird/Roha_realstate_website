4a3b00bf1e65fd5fd2dd778155afc560e97bbdf2
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-16 17:46:33 +0300

    fresh start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-red-400'>This is a start screen</h1>
+    </div>
+  );
+}
+
+export default App;
