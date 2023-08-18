b216c9eae73e9a343d04dd5b908027fa07ec57b1
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-18 19:31:29 +0300

    nav-bar hover effect added

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -3,16 +3,16 @@ import Logo from '../assets/roha-logo.png'
 
 export const Navbar = () => {
   return (
-    <div className='flex justify-around pt-3 bg-gray-700 fixed w-full z-50'>
+    <div className='flex justify-around py-1 bg-blue-900 bg-opacity-70 fixed w-full z-50'>
        <div className='w-1/12'>
         <img src={Logo} alt="" />
         </div>
         <div className='space-x-8 text-orange-300 text-xl'>
-            <a className='' href="">Residencies</a>
-            <a href="">Our Value</a>
-            <a href="">Contact Us</a>
-            <a href="">Get Started</a>
-            <button className='bg-orange-400 text-white text-base rounded px-3 py-1'>Contact</button>
+            <a className=' hover:shadow-lg pb-4 px-1 rounded hover:bg-blue-900 hover:shadow-orange-500' href="">Residencies</a>
+            <a className=' hover:shadow-lg pb-4 px-1 rounded hover:bg-blue-900 hover:shadow-orange-500' href="">Our Value</a>
+            <a className=' hover:shadow-lg pb-4 px-1 rounded hover:bg-blue-900 hover:shadow-orange-500' href="">Contact Us</a>
+            <a className=' hover:shadow-lg pb-4 px-1 rounded hover:bg-blue-900 hover:shadow-orange-500' href="">Get Started</a>
+            <button className='bg-orange-400 hover:scale-110 transition-all duration-200 text-white text-base rounded px-3 py-1'>Contact</button>
             </div> 
     </div>
   )
