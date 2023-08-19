c10462b06c1145f108c98496baccea4773d81227
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-19 09:00:23 +0300

    accordion functionality made

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hero2 from '../assets/hero/hero-2.jpg'
 import {ShieldCheck, CaretCircleDoubleDown} from "@phosphor-icons/react";
+import {Accordion_json} from '../utils/accordion_about';
 
 
 const About = () => {
+
+const [selected, setSelected] = useState(0);
+
+
+const toggleSelection = (i) =>{
+    setSelected(i);
+}
+
   return (
     <div className='my-14 grid grid-cols-2'>
         <div className=''>
@@ -16,50 +25,23 @@ const About = () => {
                 <p className='w-3/4 text-gray-400 font-semibold'>Indulge in a life of unmatched luxury with our world-class amenities. Immerse yourself in the blissful sanctuary of private pools...</p>
             </div>
 
-            <div className='mt-6 py-6 shadow-lg shadow-orange-200 border-t-2  border-r-2 rounded'>
-                <div className='flex justify-around'>
-                    <div className='text-blue-700 rounded p-2 my-auto bg-gray-200 '>
-                        <ShieldCheck size={18} />
-                    </div>
-                    <h1 className='text-blue-700 text-2xl font-semibold'>Best interest rates on the market</h1>
-                    <div className='text-blue-500 rounded p-2 bg-gray-200'>
-                        <CaretCircleDoubleDown size={18}/>
-                    </div>
-                </div>
-                <div className='pt-10 px-16 text-gray-600'>
-                    <p>Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.</p>
-                </div>
-            </div>
+        {Accordion_json.map((item, i)=>(
+ <div className='mt-6 py-6 shadow-lg shadow-orange-200 border-t-2  border-r-2 rounded'>
+ <div className='flex justify-around'>
+     <div className='text-blue-700 rounded p-2 my-auto bg-gray-200 '>
+         <ShieldCheck size={18} />
+     </div>
+     <h1 className='text-blue-700 text-2xl font-semibold'>{item.title}</h1>
+     <div className='text-blue-500 rounded p-2 bg-gray-200 cursor-pointer' onClick={()=>toggleSelection(i)}>
+         <CaretCircleDoubleDown size={18}/>
+     </div>
+ </div>
+ <div className='pt-10 px-16 text-gray-600'>
+     <p className='transition-all duration-200 ease-in-out'>{selected == i && item.description}</p>
+ </div>
+</div>
 
-            <div className='mt-6 py-6 shadow-lg shadow-orange-200 border-t-2  border-r-2 rounded'>
-                <div className='flex justify-around'>
-                    <div className='text-blue-700 rounded p-2 my-auto bg-gray-200 '>
-                        <ShieldCheck size={18} />
-                    </div>
-                    <h1 className='text-blue-700 text-2xl font-semibold'>Best interest rates on the market</h1>
-                    <div className='text-blue-500 rounded p-2 bg-gray-200'>
-                        <CaretCircleDoubleDown size={18}/>
-                    </div>
-                </div>
-                <div className='hidden pt-10 px-16 text-gray-600'>
-                    <p>Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.</p>
-                </div>
-            </div>
-
-            <div className='mt-6 py-6 shadow-lg shadow-orange-200 border-t-2  border-r-2 rounded'>
-                <div className='flex justify-around'>
-                    <div className='text-blue-700 rounded p-2 my-auto bg-gray-200 '>
-                        <ShieldCheck size={18} />
-                    </div>
-                    <h1 className='text-blue-700 text-2xl font-semibold'>Best interest rates on the market</h1>
-                    <div className='text-blue-500 rounded p-2 bg-gray-200'>
-                        <CaretCircleDoubleDown size={18}/>
-                    </div>
-                </div>
-                <div className='hidden pt-10 px-16 text-gray-600'>
-                    <p>Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.</p>
-                </div>
-            </div>
+        ))}
 
 
         </div>
@@ -67,4 +49,6 @@ const About = () => {
   )
 }
 
-export default About
+export default About;
+
+
---
+++ src/utils/accordion_about.js
@@ -0,0 +1,14 @@
+export const Accordion_json = [
+    {
+        title: "Best interest rates on the market",
+        description: "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim."
+    },
+    {
+        title: "Best interest rates on the market",
+        description: "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim."
+    },
+    {
+        title: "Best interest rates on the market",
+        description: "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim."
+    }
+];
