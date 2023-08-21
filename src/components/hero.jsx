41014d3f422fb61af88341caa692286160096a16
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-21 11:11:58 +0300

    hero section responsivness added

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,43 +4,45 @@ import Hero1 from '../assets/hero/hero-1.jpg';
 
  export const Hero = () => {
   return (
-    <div className='grid grid-cols-2 gap-x-6 px-10 bg-blue-900 py-20'>
+    <div className='lg:grid grid-cols-2 gap-x-6 px-10 bg-blue-900 py-10 md:py-20'>
 
-        <div className='col-span-1 pt-10'>
-            <h1 className='text-7xl text-white w-3/4 font-semibold'>
+        <div className='col-span-1 pt-10 w-full  '>
+            <h1 className='mx-auto text-4xl md:text-7xl text-white  md:w-3/4 font-semibold'>
                 Discover <br></br> Most Suitable Property
             </h1>
             
-            <div className='pt-24 text-gray-300'>
-            <p >Find a variety of properties that suit you very easily</p>
-            <p>Forger all difficulties in finding a residence for you</p>
+            <div className='  text-center md:block pt-12 md:pt-24 text-gray-300 w-full  '>
+            <p className='' >Find a variety of properties that suit you very easily</p>
+            <p className=''>Forger all difficulties in finding a residence for you</p>
             </div>
       
-            <div className='flex mt-7 bg-white px-7 py-4 w-fit space-x-7 rounded shadow-lg shadow-orange-300'>
-                <div className='text-orange-600 font-bold'>
+            <div className='mx-auto flex mt-7 bg-white md:px-7 px-2 py-4 w-fit space-x-2 md:space-x-7 rounded shadow-lg shadow-orange-300'>
+                <div className='text-orange-600 font-bold '>
                     <MapPin  size={32}/>
                 </div>
-                <input type="text" name="" id="" />
+                <div className='w-1/2 items-center my-auto'>
+                <input type="text" name="" id="" size={10}/>
+                </div>
                 <button className='bg-orange-600 px-4 py-2 text-white rounded hover:scale-110 transition-all duration-200'>Search</button>
               
             </div>
 
 
-            <div className='py-10 flex space-x-8 text-white'>
-                <div>
-                    <div className='flex text-4xl'>
+            <div className='py-10 flex space-x-8 text-white w-fit mx-auto'>
+                <div >
+                    <div className='flex text-2xl md:text-4xl'>
                     <h1 className='pr-2'>9,000</h1> <h1 className='text-orange-400'> +</h1>
                     </div>
                     <p>Premiun Product</p>
                 </div>
                 <div>
-                    <div className='flex text-4xl'>
+                    <div className='flex text-2xl md:text-4xl'>
                     <h1 className='pr-2'> 2,000</h1> <h1 className='text-orange-400'> +</h1>
                     </div>
                     <p>Happy Customer</p>
                 </div>
                 <div>
-                    <div className='flex text-4xl'>
+                    <div className='flex text-2xl md:text-4xl'>
                     <h1 className='pr-2'>28</h1> <h1 className='text-orange-400'> +</h1>
                     </div>
                     <p>Awards Winning</p>
@@ -49,7 +51,7 @@ import Hero1 from '../assets/hero/hero-1.jpg';
         </div>
 
         <div>
-               <div className=' px-10'>
+               <div className='w-fit mx-auto px-10'>
                 <img className='rounded-t-full border-4  border-orange-400' src={Hero1} alt="" />
                 </div> 
 
