d5d0377da8721a7d7c4b9ce80f637d60f7ea500e
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-21 11:39:54 +0300

    brands and slider responsivness added

--- src/components/brands.jsx
+++ src/components/brands.jsx
@@ -6,12 +6,13 @@ import Brand4 from '../assets/brand/brand4.png';
 
 export const Brands = () => {
   return (
-    <div className=' px-5 py-5 w-full'>
-<div className='flex  space-x-10 justify-around'>
-<img className='w-fit ' src={Brand1} alt="" />
-<img className='w-fit' src={Brand2} alt="" />
-<img className='w-fit' src={Brand3} alt="" />
-<img className='w-fit' src={Brand4} alt="" />
+    <div className=' md:px-5 py-5 w-full'>
+<div className=' md:flex  md:space-x-10 justify-around w-fit mx-auto'>
+
+<img className='w-fit  ' src={Brand1} alt="" />
+<img className='w-fit   ' src={Brand2} alt="" />
+<img className='w-fit ' src={Brand3} alt="" />
+<img className='w-fit  ' src={Brand4} alt="" />
 </div>
     </div>
   )
--- src/components/show-homes.jsx
+++ src/components/show-homes.jsx
@@ -10,15 +10,30 @@ import {Residents} from '../utils/residents'
 
 export const ShowHomes = () => {
 
+
+  const breakpoints = {
+    935: {
+      slidesPerView: 4,
+    },
+    746: {
+      slidesPerView: 3,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+  
+  };
+
+
   return (
-    <div className='px-5  '>
+    <div className='px-5 text-center md:text-left '>
         <h1 className='text-orange-400 text-2xl font-semibold'>Best Choices</h1>
         <h1 className='text-blue-900 text-3xl font-bold'>Popular Residencies</h1>
 
         <div className='relative flex py-5 justify-end' >
         
 
-<div className='text-xl space-x-5 pr-20'>
+<div className='text-xl space-x-5 md:pr-20 mx-auto md:-mx-0 '>
         <button  className='review-swiper-button-prev text-blue-500 py-1 px-2 shadow-xl border-t-2 border-l-2 border-orange-300 shadow-orange-400  rounded-lg'>{"<"}</button>
         <button  className='review-swiper-button-next text-blue-500 py-1 px-2 shadow-xl border-t-2 border-l-2 border-orange-300 shadow-orange-400  rounded-lg'>{">"}</button>
     </div>
@@ -35,9 +50,12 @@ export const ShowHomes = () => {
         prevEl: '.review-swiper-button-prev',
       }}
       spaceBetween={50}
-      slidesPerView={4}
+     
+     breakpoints={breakpoints}
+      
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
+    
           >
 
 
@@ -48,9 +66,11 @@ export const ShowHomes = () => {
     
    
 <div className='px-8 mb-12 rounded-2xl py-4 hover:scale-105 trasition-all duration-1000 hover:bg-gradient-to-t from-blue-200 to-white'>
-    <img className='rounded-2xl' src={resident.image} alt="" />
+    <img className='mx-auto rounded-2xl' src={resident.image} alt="" />
   <div className='flex gap-1 text-lg font-bold p-2 text-gray-500'> 
-    <h1 className='text-blue-600'>$</h1> <h1>{resident.price}</h1>
+    <div className='mx-auto flex'>
+    <h1 className= ' text-blue-600'>$</h1> <h1>{resident.price}</h1>
+    </div>
   </div>
     <h1 className=' text-orange-400 text-2xl font-bold'>{resident.place}</h1>
     <p className=' text-xs text-gray-500 font-semibold'>{resident.description}</p>
